72adf9b1e934ca7cb166ea53b065173134a9fd81
AlpinooO <alpino.cdc@gmail.com>
2025-01-17 10:39:10 +0100

    Ajout joueur

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 import * as THREE from "three";
 const scene = new THREE.Scene();
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x11eef4 });
-const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-boxMesh.position.x = 0;
-boxMesh.position.y = 0;
-boxMesh.position.z = 0;
+const player1Geometry = new THREE.BoxGeometry(0.1, 1.5, 0.5);
+const player1Material = new THREE.MeshBasicMaterial({ color: "white" });
+const player1Mesh = new THREE.Mesh(player1Geometry, player1Material);
+player1Mesh.position.x = -4;
+player1Mesh.position.y = 0;
+player1Mesh.position.z = 0;
 
-scene.add(boxMesh);
+scene.add(player1Mesh);
+
+const player2Geometry = new THREE.BoxGeometry(0.1, 1.5, 0.5);
+const player2Material = new THREE.MeshBasicMaterial({ color: "white" });
+const player2Mesh = new THREE.Mesh(player2Geometry, player2Material);
+player2Mesh.position.x = 4;
+player2Mesh.position.y = 0;
+player2Mesh.position.z = 0;
+
+scene.add(player2Mesh);
+
+// mettre le truc en plein écran
+const viewportSize = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  viewportSize.width / viewportSize.height
+);
+scene.add(camera);
+
+const canvas = document.querySelector("canvas#webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(viewportSize.width, viewportSize.height);
 
 camera.position.z = 3;
 renderer.render(scene, camera);
